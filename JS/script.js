b324c037de21771ed4f7b73d5d84ad35e4cf2f19
Kowiel <86078928+Kowiel@users.noreply.github.com>
2023-02-20 23:18:47 +0100

    The Simple JS Counter

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+let countEl = document.getElementById("count-el")
+let welcomeEl = document.getElementById("Saved")
+let TatalCountEl = document.getElementById("TatalCount")
+
+let count = 0
+let total=0;
+
+let TotalText=TatalCountEl.textContent;
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+// 1. Create a function, save(), which logs out the count when it's called
+
+function save(){
+
+    let content=" "+count+" - ";
+    
+    welcomeEl.textContent+=content;
+    SetCountToZeroAndSunm();
+    TatalCountEl.innerText=TotalText+" "+total;
+    
+    
+    
+}
+
+function SetCountToZeroAndSunm()
+{
+    total+=count;
+    count=0;
+    countEl.textContent=count;
+}
+
+
+
+
